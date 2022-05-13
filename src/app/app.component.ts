b292f2b0f55355ace5ebf88dc45c8d86ff65a1ea
abhisheksahanii <abhishek1sahani@gmail.com>
2022-05-13 17:07:07 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
 
+export class Movies{
+  constructor(
+  public id : number,
+  public globalrank : string,
+  public title : string
+  ){
+
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+
+  movies: Movies[] | undefined;
+  constructor(
+    private httpClient: HttpClient
+  ){}
+
+  ngOnInit():void{
+    this.getMovies();
+  }
+
+  getMovies(){
+    this.httpClient.get<any>('http://localhost:3000/movies').subscribe(
+      response =>{
+        console.log(response);
+        this.movies = response;
+        
+      }
+     );
+  }
 }
